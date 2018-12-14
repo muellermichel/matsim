566bcb6e1c62068de25b66fcb23d22f4b92c50e2
mmu@ategra.ch <mmueller.ee@gmail.com>
2018-12-14 10:03:17 +0100

    new QuickEvents class that can be used to generate minimal event data based on just two integers per event

--- matsim/src/main/java/org/matsim/core/utils/quickevents/QuickEvents.java
+++ matsim/src/main/java/org/matsim/core/utils/quickevents/QuickEvents.java
@@ -1,5 +1,28 @@
 package org.matsim.core.utils.quickevents;
 
+import java.nio.ByteBuffer;
+
 public class QuickEvents {
-    //TODO
+    private ByteBuffer buf;
+
+    public QuickEvents(int maxNumPlanElements, int maxNumTicks) {
+        buf = ByteBuffer.allocate(4 * 2 * maxNumPlanElements + maxNumTicks);
+    }
+
+    public void tick() {
+        buf.put((byte) -1);
+    }
+
+    public void clear() {
+        buf.clear();
+    }
+
+    public void registerEvent(int agentId, int planIndex) {
+        buf.putInt(agentId);
+        buf.putInt(planIndex);
+    }
+
+    public byte[] getData() {
+        return buf.array();
+    }
 }
