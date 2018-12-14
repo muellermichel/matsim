3a75146f45ee17c8661ed86fcf9c47a172b7dff9
mmu@ategra.ch <mmueller.ee@gmail.com>
2018-12-14 10:14:45 +0100

    improved design of quickevents class so reading out becomes easier / faster

--- matsim/src/main/java/org/matsim/core/utils/quickevents/QuickEvents.java
+++ matsim/src/main/java/org/matsim/core/utils/quickevents/QuickEvents.java
@@ -6,18 +6,18 @@ public class QuickEvents {
     private ByteBuffer buf;
 
     public QuickEvents(int maxNumPlanElements, int maxNumTicks) {
-        buf = ByteBuffer.allocate(4 * 2 * maxNumPlanElements + maxNumTicks);
+        buf = ByteBuffer.allocate(4 * (2 * maxNumPlanElements + maxNumTicks));
     }
 
     public void tick() {
-        buf.put((byte) -1);
+        buf.putInt(-1);
     }
 
     public void clear() {
         buf.clear();
     }
 
-    public void registerEvent(int agentId, int planIndex) {
+    public void registerPlannedEvent(int agentId, int planIndex) {
         buf.putInt(agentId);
         buf.putInt(planIndex);
     }
