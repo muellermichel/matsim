b4b06d797aff68d694786ba81bf21252539d7b5d
mmu@ategra.ch <mmueller.ee@gmail.com>
2019-01-11 16:54:20 +0100

    fixes to make it run till validation on headless servers

--- Run.sh
+++ Run.sh
@@ -16,17 +16,23 @@ echo "======RUN ALL THE THINGS========="
 #config=$script_dir/scenarios/berlin-v5.1-1pct/input/berlin-v5.1-1pct-1it.config.xml
 
 config=$script_dir/examples/scenarios/berlin-v5.1-1pct-1agent/input/berlin-v5.1.config.xml
+
 #config=$script_dir/examples/scenarios/berlin-v5.1-1pct-1pt/input/berlin-v5.1.config.xml
 #config=$script_dir/examples/scenarios/berlin-v5.1-1pct-1pt-1agent/input/berlin-v5.1.config.xml
 #config=$script_dir/examples/scenarios/berlin-v5.1-1pct-fullpt/input/berlin-v5.1.config.xml
 #config=$script_dir/examples/scenarios/berlin-v5.1-1pct/input/berlin-v5.1.config.xml
 
 timestamp=$(date +"%Y-%m-%d-%H_%M")
+config_dir="$(dirname $config)/.."
+config_basename="$(basename $(dirname $(dirname $config)))"
+validation_path="${config_dir}/output/ITERS/it.0/${config_basename}.0.events.xml.gz"
+
+echo "Running matsim with config ${config} in ${config_dir}, base ${config_basename}, validating against ${validation_path}"
 
 java \
 	-Xmx15G \
 	-Dfile.encoding=UTF-8 \
 	-classpath "${classpath}" \
-	org.matsim.berlin.RunBerlinScenario $config | tee output_${timestamp}.txt | tee run.log
+	org.matsim.berlin.RunBerlinScenario "${config}" "" "${validation_path}"  | tee output_${timestamp}.txt | tee run.log
 
 echo "Running matsim...Done!"
