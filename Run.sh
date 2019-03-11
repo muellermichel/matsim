1a7dcc105b5ee0bb598824650a658330cb245788
Rodrigo Bruno <rbruno@gsd.inesc-id.pt>
2019-03-11 17:17:45 +0100

    added scripts to facilitate testing.

--- Run.sh
+++ Run.sh
@@ -2,6 +2,9 @@
 
 script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+java=$JAVA_HOME/bin/java
+
 # To get the classpath by maven:
 mvn -pl matsim dependency:build-classpath -Dmdep.outputFile=matsim.cp
 classpath="\
@@ -9,30 +12,41 @@ $script_dir/matsim/target/matsim-0.11.0-SNAPSHOT-tests.jar:\
 $script_dir/matsim/target/matsim-0.11.0-SNAPSHOT.jar:\
 $(cat $script_dir/matsim/matsim.cp)"
 
-echo "============CLASSPATH============"
-echo $classpath
-echo "======RUN ALL THE THINGS========="
-
 #config=$script_dir/scenarios/berlin-v5.1-1pct/input/berlin-v5.1-1pct-1it.config.xml
 
-config=$script_dir/examples/scenarios/berlin-v5.1-1pct-1agent/input/berlin-v5.1.config.xml
+# How to test:
+# 1) turn on debug (Realm.java)
+# 2) cat run.log | grep "Processed 1" | wc -l
 
+# Should yield 102 network interactions
+#config=$script_dir/examples/scenarios/berlin-v5.1-1pct-1agent/input/berlin-v5.1.config.xml
+# Should yield 36 network interactions
 #config=$script_dir/examples/scenarios/berlin-v5.1-1pct-1pt/input/berlin-v5.1.config.xml
 #config=$script_dir/examples/scenarios/berlin-v5.1-1pct-1pt-1agent/input/berlin-v5.1.config.xml
 #config=$script_dir/examples/scenarios/berlin-v5.1-1pct-fullpt/input/berlin-v5.1.config.xml
-#config=$script_dir/examples/scenarios/berlin-v5.1-1pct/input/berlin-v5.1.config.xml
+config=$script_dir/examples/scenarios/berlin-v5.1-1pct/input/berlin-v5.1.config.xml
 
-timestamp=$(date +"%Y-%m-%d-%H_%M")
-config_dir="$(dirname $config)/.."
-config_basename="$(basename $(dirname $(dirname $config)))"
-validation_path="${config_dir}/output/ITERS/it.0/${config_basename}.0.events.xml.gz"
+# Overriding configs. Used to specify the number of sim threads
+over_prefix=$script_dir/examples/scenarios/berlin-v5.1-1pct/input
 
-echo "Running matsim with config ${config} in ${config_dir}, base ${config_basename}, validating against ${validation_path}"
+function run {
+    echo "Running matsim..."
+    $java \
+    	-Xmx15G \
+    	-Dfile.encoding=UTF-8 \
+    	-classpath "${classpath}" \
+    	org.matsim.berlin.RunBerlinScenario $config $over &> run.log
+    echo "Running matsim...Done!"
+    grep "ETHZ" run.log | grep "Done"
+}
 
-java \
-	-Xmx15G \
-	-Dfile.encoding=UTF-8 \
-	-classpath "${classpath}" \
-	org.matsim.berlin.RunBerlinScenario "${config}" "" "${validation_path}"  | tee output_${timestamp}.txt | tee run.log
-
-echo "Running matsim...Done!"
+echo "Config = $config"
+#for over_suffix in 1-sim-threads 2-sim-threads 4-sim-threads 8-sim-threads
+for over_suffix in 1-sim-threads
+do
+    over=$over_prefix/$over_suffix.xml
+    echo "Over = $over"
+    run
+done
+paplay /usr/share/sounds/freedesktop/stereo/complete.oga
+beep
